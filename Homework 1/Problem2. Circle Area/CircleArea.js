c2d4e71df8d9af6daecd1af92e6e5decb64eb53f
Radoslav Minchev <r.minchev@yahoo.com>
2015-07-07 14:52:21 +0300

    - Problem2. Circle Area/index.html final

--- Homework 1/Problem2. Circle Area/CircleArea.js
+++ Homework 1/Problem2. Circle Area/CircleArea.js
@@ -4,3 +4,4 @@ return (r * r *Math.PI);
 document.getElementById("circle1").innerHTML = calcCircleArea(7);
 document.getElementById("circle2").innerHTML = calcCircleArea(1.5);
 document.getElementById("circle3").innerHTML = calcCircleArea(20);
+console.log('rado');
